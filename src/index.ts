6a915a3cfe349e47739a990dcae1bc932efe4f58
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-07-12 14:26:47 -0300

    feat: cria tipagem do contexto para usar no resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+import { Context } from '@schema/context'
 import { resolvers, typeDefs } from '@schema/index'
+import { UserDataSource } from '@schema/user/dataSources'
 
-const server = new ApolloServer({
+const server = new ApolloServer<Context>({
   typeDefs,
   resolvers,
 })
@@ -11,6 +13,15 @@ const server = new ApolloServer({
 async function startServer() {
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async ({ req, res }) => {
+      return {
+        req,
+        res,
+        dataSources: {
+          user: new UserDataSource(),
+        },
+      }
+    },
   })
   console.log(`🚀 Server ready at: ${url}`)
 }
